cdd8bd45362b4538554c383b01bf6b4985d359db
flobee <flobee@r61>
2015-09-14 17:13:50 +0200

    Updates createApi using phpdoc from version 1.4.4 to run with 2.8.*

--- tests/createApi.sh
+++ tests/createApi.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-cmd=/usr/bin/phpdoc; # ~/scripts/lib/pear/PhpDocumentor-1.4.3/phpdoc
+# hints:
+# ~/scripts/lib/pear/PhpDocumentor-1.4.3/phpdoc
+# phpdoc 1.4.4
+# phpdoc --title "Mumsys library" -o "HTML:frames:earthli" -t ../docs/API/ \
+#   --directory ../src/ --ignore "Zend/*,Smarty/*,getid3/*,phpthumb/*,graph/*"
+#
+# phpdoc >= 2.8:
+# --template responsive-twig|clean
+
+cmd=/usr/bin/phpdoc;
 echo 'building API';
 
 # --parseprivate
 $cmd \
 --title "Mumsys library" \
--o HTML:frames:earthli \
+--template="responsive" \
 -t ../docs/API/ \
 --directory ../src/ \
---ignore \
-Zend/* \
-Smarty/*,\
-getid3/*,\
-phpthumb/*,\
-graph/*
+--ignore "Zend/*,Smarty/*,getid3/*,phpthumb/*,graph/*"
 
