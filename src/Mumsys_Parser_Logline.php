a9266c0af579b40e58240859d7c72befec5c713a
flobee <flobee@r61>
2016-01-22 16:54:15 +0100

    Adds parser for log/ data lines; Adds tests: code coverage = 100%

---
+++ src/Mumsys_Parser_Logline.php
@@ -0,0 +1,379 @@
+<?php
+
+/*{{{*/
+/**
+ * ----------------------------------------------------------------------------
+ * Mumsys_Parser_Logline
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * ----------------------------------------------------------------------------
+ * @copyright Copyright (c) 2015 by Florian Blasel for FloWorks Company
+ * ----------------------------------------------------------------------------
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Parser
+ * @version     1.1.1
+ * Created: 2015-08-11
+ * @filesource
+ */
+/*}}}*/
+
+
+/**
+ * Class to parse a log line into it's parts from a common log file like apache
+ * or nginx or by given special format.
+ *
+ * Example:
+ * <pre>
+ * # apache vhost configuration
+ * $logFormat = '%v:%p %h %l %u %t "%r" %>s %O "%{Referer}i" "%{User-Agent}i"';
+ * $logFile = '/tmp/other_vhosts_access.log';
+ * // master patterns to be set or defaults take affect
+ * $patterns = array();
+ * $parser = new Mumsys_Parser_Logfile($logFormat, $patterns);
+ * // is already default: $parser->setFilterCondition('AND');
+ * // is already default: $parser->setShowFilterResults();
+ * // show the opposite, all which not match to the filters
+ * // $parser->setHideFilterResults();
+ * // show only http code 304
+ * $parser->addFilter('httpcode', '304', true);// note: "30" matches to 300 301...
+ * // check only port 80
+ * $parser->addFilter('port', '80', true);
+ * $file = new SplFileObject($logFile);
+ * while (!$file->eof())  {
+ *      $line = $file->fgets();
+ *      $record = $parser->parse($line);
+ * </pre>
+ *
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Parser
+ */
+class Mumsys_Parser_Logline
+{
+    /**
+     * Version ID information
+     */
+    const VERSION = '1.1.1';
+
+    /**
+     * Default log format.
+     *
+     * E.g.: Check your server config for details of the format:
+     * Apache e.g "vhost_combined", "combined", "common" or just "referer":
+     * vhost_combined:   "%v:%p %h %l %u %t \"%r\" %>s %O "%{Referer}i" "%{User-Agent}i"
+     * combined:        "%h %l %u %t \"%r\" %>s %O "%{Referer}i" "%{User-Agent}i"
+     * common:          "%h %l %u %t \"%r\" %>s %O"
+     * referer:         "%{Referer}i -> %U"
+     * agent:            "%{User-agent}i"
+     * @var string
+     */
+    private $_defaultLogFormat = '%h %l %u %t "%r" %>s %O';
+
+    /**
+     * Log format to be used internally
+     * @var string
+     */
+    private $_logFormat;
+
+    /**
+     * List of filters
+     * @var array
+     */
+    private $_filters = array();
+
+    /**
+     * Flag for the way the filter will perform.
+     * By default (false) the filter is set as whitelist which means only matches of the filter will return.
+     * @var boolean
+     */
+    private $_filteredHide = false;
+
+    /**
+     * Flag to change the filter/ search behavior.
+     * By default and condition will be used (all filter must match for an entry)
+     * @var boolean
+     */
+    private $_filtersInAndConditions = true;
+
+
+    /**
+     * Default pattern list.
+     * List of key/value pairs to substitute the key with the regular
+     * expression. These are common patterns to be used in apache or nginx.
+     *
+     * @var array
+     */
+    private $_patternsDefault = array(
+        '%%' => '(?P<percent>\%)',
+        '%a' => '(?P<remoteIP>(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))|([0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4}){7})|(([0-9A-Fa-f]{1,4})?(:[0-9A-Fa-f]{1,4}){0,7}:(:[0-9A-Fa-f]{1,4}){1,7}))',
+        '%A' => '(?P<localIP>(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))|([0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4}){7})|(([0-9A-Fa-f]{1,4})?(:[0-9A-Fa-f]{1,4}){0,7}:(:[0-9A-Fa-f]{1,4}){1,7}))',
+
+        '%h' => '(?P<host>[a-zA-Z0-9\-\._:]+)',
+
+        '%m' => '(?P<method>OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT)',
+        '%p' => '(?P<port>\d+)',
+        '%r' => '(?P<request>(?:(?:[A-Z]+) .+? HTTP/1.(?:0|1))|-|)',
+
+        '%u' => '(?P<user>(?:-|[\w-]+))',
+        '%U' => '(?P<url>.+?)',
+        '%v' => '(?P<serverName>([a-zA-Z0-9]+)([a-z0-9.-]*))',
+        '%V' => '(?P<canonicalServerName>([a-zA-Z0-9]+)([a-z0-9.-]*))',
+
+        '%>s' => '(?P<httpcode>\d{3}|-)',
+        '%b' => '(?P<respBytes>(\d+|-))',
+        '%T' => '(?P<reqTime>(\d+\.?\d*))',
+        '%O' => '(?P<txBytes>[0-9]+)',
+        '%I' => '(?P<rxBytes>[0-9]+)',
+
+        '\%\{(?P<name>[a-zA-Z]+)(?P<name2>[-]?)(?P<name3>[a-zA-Z]+)\}i' => '(?P<Header\\1\\3>.*?)',
+        '%D' => '(?P<timeServeRequest>[0-9]+)',
+        '%t' => '\[(?P<time>\d{2}/(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/\d{4}:\d{2}:\d{2}:\d{2} (?:-|\+)\d{4})\]',
+        '%l' => '(?P<logname>(?:-|[\w-]+))',
+    );
+
+    /**
+     * Patterns list to be used internally
+     * @var array
+     */
+    private $_patterns = array();
+
+
+    /**
+     * Initialise the object with a format an if needed your own configuration
+     * of patterns.
+     *
+     * @param string $format Format of a log line.
+     * @param array $patterns Initial patterns to be set. Otherwise default patterns will be used
+     */
+    public function __construct($format='', array $patterns=array())
+    {
+        if ($patterns) {
+            $this->_patterns = $patterns;
+        } else {
+            $this->_patterns = $this->_patternsDefault;
+        }
+
+        if ($format) {
+            $this->setFormat($format);
+        } else {
+            $this->setFormat($this->_defaultLogFormat);
+        }
+    }
+
+
+    /**
+     * Set the format of a log line.
+     *
+     * @param string $format
+     */
+    public function setFormat($format)
+    {
+        $this->_logFormat = "#^{$format}$#";
+    }
+
+
+    /**
+     * Returns the reqular expression based on format and patterns.
+     */
+    private function _getExpression()
+    {
+        $expr = $this->_logFormat;
+
+        foreach ($this->_patterns as $key => $replace) {
+            $expr = preg_replace("/{$key}/", $replace, $expr);
+        }
+        return $expr;
+    }
+
+
+    /**
+     * Sets or adds a pattern to pattern list.
+     *
+     * Default list of key/value pairs to substitute the key with the regular
+     * expression. These are common patterns to be used in apache or nginx:
+     * @see $this->_patternsDefault
+     *
+     * @param string $key Pattern identifier
+     * @param string $pattern The pattern/ expression
+     */
+    public function setPattern($key, $pattern)
+    {
+        $this->_patterns[$key] = $pattern;
+    }
+
+
+    /**
+     * Set the filters to return all NONE matching items from the filters.
+     *
+     * Note: This works only if filters are set.
+     */
+    public function setHideFilterResults()
+    {
+        $this->_filteredHide = true;
+    }
+
+
+    /**
+     * Set the filters to return all matches from the filters.
+     * Hint: Search for matches by given filter rules.
+     *
+     * Note: This works only if filters are set.
+     */
+    public function setShowFilterResults()
+    {
+        $this->_filteredHide = false;
+    }
+
+
+    /**
+     * Sets the filter condition mode. Apply all filters in AND or OR condition.
+     *
+     * @param string $orOrAnd Condition to be set: "AND" or "OR"; Default: "AND"
+     * @throws Exception If incoming parameter is whether the string "AND" nor "OR"
+     */
+    public function setFilterCondition($orOrAnd)
+    {
+        $chk = strtoupper($orOrAnd);
+
+        if ($chk == 'AND' || $chk == 'OR') {
+            if ($chk == 'AND') {
+                $this->_filtersInAndConditions = true;
+            } else {
+                $this->_filtersInAndConditions = false;
+            }
+        } else {
+            throw new Mumsys_Parser_Exception('Invalid filter condition');
+        }
+    }
+
+
+    /**
+     * Adds a filter/ search rule.
+     * Note: Don't forget to escape spesial chars for the regular expressions.
+     *
+     * @param string $key Keyword based on the pattern rules to be expected: eg.: httpcode, user, time ...
+     * @param array|string $value Value or list of values to look/ search for. matching tests! not exact tests!
+     * @param type $sensitive Flag to enable sensitive mode or not. Default: false (case insensitive)
+     */
+    public function addFilter($key, $value=array(), $sensitive=false)
+    {
+        if ( is_string($value) ) {
+            $value = array($value);
+        }
+
+        foreach ($value as $i => &$raw) {
+            $value[$i] = preg_quote($raw, '#');
+        }
+
+        $this->_filters[$key][] = array('values'=>$value, 'case' => $sensitive);
+    }
+
+
+    /**
+     * Parse a log line and return its parts.
+     *
+     * @param string $line A line of the log file.
+     *
+     * @return array|false Returns array with found properties or empty array if
+     * filters take affect or false for an empty line.
+     * @throws Exception If format doesn't match the line format.
+     */
+    public function parse($line)
+    {
+        if (empty($line)) {
+            return false;
+        }
+
+        $regex = $this->_getExpression();
+
+        if (!preg_match($regex, $line, $matches)) {
+            $message = sprintf(
+                'Format of log line invalid (expected:"%1$s"); Line was "%2$s"; regex: "%3$s"',
+                $this->_logFormat,
+                $line,
+                $regex
+            );
+            throw new Mumsys_Parser_Exception($message);
+        }
+
+        $result = array();
+
+        foreach (array_filter(array_keys($matches), 'is_string') as $key) {
+            if ('time' === $key && true !== $stamp = strtotime($matches[$key])) {
+                $result['stamp'] = $stamp;
+            }
+
+            $result[$key] = $matches[$key];
+        }
+
+        $return = array();
+        if ( ($ok=$this->_applyFilters($result)) ) {
+            $return = $ok;
+        }
+
+        return $return;
+    }
+
+
+    /**
+     * Apply filters.
+     *
+     * @param array $result list of parameters from the log line.
+     * @return array Retruns an empty array if filters take affect otherwise the
+     * it returns the incoming result array
+     */
+    private function _applyFilters($result)
+    {
+        if (!$this->_filters) {
+            return $result;
+        }
+
+        $numMatches = 0;
+        $itMatchesInOrCondition = false;
+
+        foreach( $this->_filters as $key => $paramsList )
+        {
+            if (isset($result[$key]) && $result[$key] )
+            {
+                foreach( $paramsList as $i => $params )
+                {
+                    foreach( $params['values'] as $value )
+                    {
+                        $modifier = 'i';
+                        if ( $params['case'] ) {
+                            $modifier = '';
+                        }
+
+                        $regex = sprintf('/(%1$s)/%2$s', $value, $modifier);
+                        if ( preg_match($regex, $result[$key]) ) {
+                            $numMatches += 1;
+                            $itMatchesInOrCondition = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($this->_filtersInAndConditions)
+        {
+            $itMatches = false;
+            if (count($this->_filters) == $numMatches) {
+                $itMatches = true;
+            }
+        } else {
+            $itMatches = $itMatchesInOrCondition;
+        }
+
+        if ($itMatches === $this->_filteredHide) {
+            return array();
+        } else {
+            return $result;
+        }
+    }
+
+}
+
---
+++ tests/src/Mumsys_Parser_LoglineTest.php
@@ -0,0 +1,277 @@
+<?php
+
+
+/**
+ * Test class for Mumsys_Parser_Logline
+ */
+class Mumsys_Parser_LoglineTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Mumsys_Parser_Logline
+     */
+    protected $_object;
+    protected $_format;
+    protected $_patterns;
+
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->_logContent = '1;whichhas;semicolon;as;delimiter;opt' . PHP_EOL
+            . '2;which has;semicolon;as;delimiter;opt' . PHP_EOL
+            . '33;"which has";"semicolon";"as";"delimiter";' . PHP_EOL
+            . '' . PHP_EOL
+            . '5;"which has";"semicolon";"as";"d";"wont work because of the quotes"' . PHP_EOL
+        ;
+
+        $this->_format = 'id;c1;c2;c3;c4;c5';
+        $this->_patterns = array(
+            'id' => '(?P<id>\w+)', // match number
+            'c1' => '(?P<col_1>.+)',
+            'c2' => '(?P<col_2>.+)',
+            'c3' => '(?P<col_3>.+)',
+            'c4' => '(?P<col_4>.+)',
+            'c5' => '(?P<col_5>\w*)', // optional
+        );
+        $this->_object = new Mumsys_Parser_Logline($this->_format, $this->_patterns);
+    }
+
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        $this->_object = NULL;
+        $this->_format = '';
+        $this->_patterns = '';
+        $this->_logContent = '';
+    }
+
+
+    /**
+     * @covers Mumsys_Parser_Logline::__construct
+     * @covers Mumsys_Parser_Logline::setFormat
+     */
+    public function test__construct()
+    {
+        // default loglines
+        $object = new Mumsys_Parser_Logline();
+        // own parameters
+        $this->_format = 'id;c1;c2;c3;c4;c5';
+        $this->_patterns = array(
+            'id' => '(?P<id>\w+)', // match number dosnt work :-?
+            'c1' => '(?P<col_1>.+)',
+            'c2' => '(?P<col_2>.+)',
+            'c3' => '(?P<col_3>.+)',
+            'c4' => '(?P<col_4>.+)',
+            'c5' => '(?P<col_5>\w*)', // optional
+        );
+        $object = new Mumsys_Parser_Logline($this->_format, $this->_patterns);
+    }
+
+
+    /**
+     * @covers Mumsys_Parser_Logline::setFormat
+     */
+    public function testSetFormat()
+    {
+        $this->_object->setFormat($this->_format);
+    }
+
+
+    /**
+     * @covers Mumsys_Parser_Logline::setPattern
+     */
+    public function testSetPattern()
+    {
+        $this->_object->setPattern('c5', '(?P<col_5>(\w*))');
+    }
+
+
+    /**
+     * @covers Mumsys_Parser_Logline::setHideFilterResults
+     */
+    public function testSetHideFilterResults()
+    {
+        $this->_object->setHideFilterResults();
+    }
+
+
+    /**
+     * @covers Mumsys_Parser_Logline::setShowFilterResults
+     */
+    public function testSetShowFilterResults()
+    {
+        $this->_object->setShowFilterResults();
+    }
+
+
+    /**
+     * @covers Mumsys_Parser_Logline::setFilterCondition
+     */
+    public function testSetFilterCondition()
+    {
+        $this->_object->setFilterCondition('AND');
+        $this->_object->setFilterCondition('OR');
+
+        $this->setExpectedException('Mumsys_Parser_Exception', 'Invalid filter condition');
+        $this->_object->setFilterCondition('FAILT');
+    }
+
+
+    /**
+     * @covers Mumsys_Parser_Logline::addFilter
+     */
+    public function testAddFilter()
+    {
+        $this->_object->addFilter('c5', 'delim', true);
+        $this->_object->addFilter('c5', array('delim', 'del'), false);
+    }
+
+
+    /**
+     * @covers Mumsys_Parser_Logline::parse
+     * @covers Mumsys_Parser_Logline::_getExpression
+     * @covers Mumsys_Parser_Logline::_applyFilters
+     */
+    public function testParse()
+    {
+        $records = explode(PHP_EOL, $this->_logContent);
+
+        $actual1 = $this->_object->parse ('id;c1;c2;c3;c4;c5'); //trim($records[2])
+        $expected1 = array(
+            'id' => 'id',
+            'col_1' => 'c1',
+            'col_2' => 'c2',
+            'col_3' => 'c3',
+            'col_4' => 'c4',
+            'col_5' => 'c5'
+        );
+        // default
+        $actual2 = $this->_object->parse(trim($records[0]));
+        $expected2 = array(
+            'id' => '1',
+            'col_1' => 'whichhas',
+            'col_2' => 'semicolon',
+            'col_3' => 'as',
+            'col_4' => 'delimiter',
+            'col_5' => 'opt'
+        );
+        // space in "which has"
+        $actual3 = $this->_object->parse(trim($records[1]));
+        $expected3 = array(
+            'id' => '2',
+            'col_1' => 'which has',
+            'col_2' => 'semicolon',
+            'col_3' => 'as',
+            'col_4' => 'delimiter',
+            'col_5' => 'opt'
+        );
+        // empty "opt",
+        $actual4 = $this->_object->parse(trim($records[2]));
+        $expected4 = array(
+            'id' => '33',
+            'col_1' => '"which has"',
+            'col_2' => '"semicolon"',
+            'col_3' => '"as"',
+            'col_4' => '"delimiter"',
+            'col_5' => ''
+        );
+        // empty line
+        $actual5 = $this->_object->parse($records[3]);
+
+        $this->assertEquals($actual1, $expected1);
+        $this->assertEquals($actual2, $expected2);
+        $this->assertEquals($actual3, $expected3);
+        $this->assertEquals($actual4, $expected4);
+        $this->assertFalse($actual5);
+
+        // crap in "opt"
+        $this->setExpectedException(
+            'Mumsys_Parser_Exception',
+            'Format of log line invalid (expected:"#^id;c1;c2;c3;c4;c5$#"); Line was "5;"which has";"semicolon";"as";'
+            . '"d";"wont work because of the quotes""; regex: "#^(?P<id>\w+);(?P<col_1>.+);(?P<col_2>.+);(?P<col_3>.+)'
+            . ';(?P<col_4>.+);(?P<col_5>\w*)$#"'
+        );
+        $actual5 = $this->_object->parse(trim($records[4]));
+    }
+
+    /**
+     * @covers Mumsys_Parser_Logline::parse
+     */
+    public function testParseTimestampFeature() {
+        // test timestamp feature
+        $line = '999;2016-01-17 07:35:14';
+        $format = 'id;timeIn';
+        $this->_object->setPattern('id', '(?P<id>\w+)');
+        // outgoing "time" is relevant for a match to generate the "stamp" key
+        $this->_object->setPattern('timeIn', '(?P<time>.+)');
+
+        $this->_object->setFormat($format);
+
+        $actual1 = $this->_object->parse($line);
+        $expected1 = array(
+            'id' => '999',
+            'time' => '2016-01-17 07:35:14',
+            'stamp' => '1453012514',
+            );
+
+        $this->assertEquals($actual1, $expected1);
+    }
+
+    /**
+     * @covers Mumsys_Parser_Logline::_applyFilters
+     */
+    public function testParseApplyFilters()
+    {
+        $line = '999;2016-01-17 07:35:14';
+
+        // test timestamp feature
+        $format = 'id;timeIn';
+        $this->_object->addFilter('time', '2016', false);
+
+        $this->_object->setPattern('id', '(?P<id>\w+)');
+        $this->_object->setPattern('timeIn', '(?P<time>.+)');
+        $this->_object->setFormat($format);
+
+        $actual1 = $this->_object->parse($line);
+        $expected1 = array(
+            'id' => '999',
+            'time' => '2016-01-17 07:35:14',
+            'stamp' => '1453012514',
+        );
+
+        // for codecoveage
+        $this->_object->setFilterCondition('OR');
+        $actual2 = $this->_object->parse($line);
+        $expected2 = array(
+            'id' => '999',
+            'time' => '2016-01-17 07:35:14',
+            'stamp' => '1453012514',
+        );
+
+        // strict filter
+        $object = new Mumsys_Parser_Logline();
+        $object->setPattern('id', '(?P<id>\w+)');
+        $object->setPattern('timeIn', '(?P<time>.+)');
+        $object->setFormat($format);
+        $object->addFilter('id', 'abc', true);
+        $actual3 = $object->parse('ABC;2016-01-17 07:35:14');
+        $expected3 = array(
+            // should not match
+        );
+
+        // hide matches (filteredHide)
+        //
+
+        $this->assertEquals($actual1, $expected1);
+        $this->assertEquals($actual2, $expected2);
+        $this->assertEquals($actual3, $expected3);
+    }
+
+}
