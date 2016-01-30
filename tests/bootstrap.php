26ae61c5012021c7a46c5998af2c01e32815dde2
flobee <flobee@r61>
2016-01-31 00:50:49 +0100

    Update include path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,7 @@ date_default_timezone_set('Europe/Berlin');
 
 setlocale(LC_ALL, 'POSIX');// "C" style
 
-require_once  __DIR__ . '../src/Mumsys_Loader.php';
+require_once  __DIR__ . '/../src/Mumsys_Loader.php';
 spl_autoload_register(array('Mumsys_Loader', 'autoload'));
 
 
