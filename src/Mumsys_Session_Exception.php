d0f17412757b99f9e8440cc88db831c92219cfa9
flobee <flobee@r61>
2016-01-30 19:18:56 +0100

    Adds session classes/tests

---
+++ src/Mumsys_Session_Exception.php
@@ -0,0 +1,35 @@
+<?php
+
+/*{{{*/
+/**
+ * ----------------------------------------------------------------------------
+ * Mumsys_Session_Exception
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * ----------------------------------------------------------------------------
+ * @copyright Copyright (c) 2015 by Florian Blasel for FloWorks Company
+ * ----------------------------------------------------------------------------
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Session
+ * @version     1.0.0
+ * Created: 2005-01-01
+ * @filesource
+ */
+/*}}}*/
+
+/**
+ * Mumsys_Session_Exception
+ *
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Session
+ */
+class Mumsys_Session_Exception extends Mumsys_Exception
+{
+
+}
+
