17d231f9a8da866d3f3d36b16c8f1992cb3cece9
flobee <flobee@r61>
2016-01-31 01:21:53 +0100

    Init Mumsys_Counter and tests; code coverage 100%

---
+++ tests/src/Mumsys_CounterTest.php
@@ -0,0 +1,127 @@
+<?php
+
+// $Id$
+
+/**
+ * Test class for Mumsys_Counter.
+ */
+class Mumsys_CounterTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Mumsys_CounterTest
+     */
+    protected $object;
+
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->_version = '3.1.0';
+        $this->_object = new Mumsys_Counter();
+    }
+
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        $this->_object = null;
+    }
+
+
+    public function testConstructor()
+    {
+        $this->_object = new Mumsys_Counter();
+        $this->assertInstanceof('Mumsys_Counter', $this->_object);
+        $this->assertEquals(0, $this->_object->result());
+
+
+        $this->_object = new Mumsys_Counter(true);
+        $this->assertInstanceof('Mumsys_Counter', $this->_object);
+        $this->assertEquals(1, $this->_object->result());
+    }
+
+
+    public function testAdd()
+    {
+        $this->_object->add(1);
+        $this->assertEquals(1, $this->_object->result());
+
+        $this->_object->add(-1);
+        $this->assertEquals(2, $this->_object->result());
+    }
+
+
+    public function testSub()
+    {
+        $this->_object->sub(1);
+        $this->assertEquals(-1, $this->_object->result());
+
+        $this->_object->sub(-1);
+        $this->assertEquals(-2, $this->_object->result());
+
+        $this->_object->sub(1.5); // positiv number: -2 -> sub 1.5 = -3.5
+        $this->assertEquals(-3.5, $this->_object->result());
+    }
+
+
+    public function testCount()
+    {
+        $this->_object->count();
+        $this->assertEquals(1, $this->_object->result());
+    }
+
+
+    public function testResult()
+    {
+        $this->assertEquals(0, $this->_object->result());
+    }
+
+    public function testtoString()
+    {
+        $this->assertEquals(0, $this->_object->__toString());
+    }
+
+
+    // test abstracts
+
+
+    /**
+     * @covers Mumsys_Counter::getVersion
+     */
+    public function testGetVersion()
+    {
+        $this->assertEquals('Mumsys_Counter ' . $this->_version, $this->_object->getVersion());
+    }
+
+    /**
+     * @covers Mumsys_Counter::getVersionID
+     */
+    public function testgetVersionID()
+    {
+        $this->assertEquals($this->_version, $this->_object->getVersionID());
+    }
+
+    /**
+     * @covers Mumsys_Counter::getVersions
+     */
+    public function testgetVersions()
+    {
+        $expected = array(
+            'Mumsys_Abstract' => '3.0.1',
+            'Mumsys_Counter' => '3.1.0',
+        );
+
+        $possible = $this->_object->getVersions();
+
+        foreach ($expected as $must => $value) {
+            $this->assertTrue(isset($possible[$must]));
+            $this->assertTrue(($possible[$must] == $value));
+        }
+    }
+}
