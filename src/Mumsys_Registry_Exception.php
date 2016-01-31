7e19400cf621d5152111fc15a57c3343d41ab54f
flobee <flobee@r61>
2016-01-31 16:33:45 +0100

    Init Mumsys_Registry and tests; codecoverage 100%

---
+++ src/Mumsys_Registry_Exception.php
@@ -0,0 +1,36 @@
+<?php
+
+/*{{{*/
+/**
+ * ----------------------------------------------------------------------------
+ * Mumsys_Registry
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * ----------------------------------------------------------------------------
+ * @copyright Copyright (c) 2014 by Florian Blasel for FloWorks Company
+ * ----------------------------------------------------------------------------
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Registry
+ * @version     1.0.0
+ * Created: 2014-01-07
+ * @filesource
+ */
+/*}}}*/
+
+
+/**
+ * Generic exception class Mumsys_Registry_Exception
+ *
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Registry
+ */
+class Mumsys_Registry_Exception extends Mumsys_Exception
+{
+
+}
+
