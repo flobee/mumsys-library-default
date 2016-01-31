59b73244aecd0ea731a5cfd22bd2bdb6b231a8c7
flobee <flobee@r61>
2016-01-31 01:37:53 +0100

    Init Mumsys_Array2Xml_Exception as part for Array2Xml handling

---
+++ src/Mumsys_Array2Xml_Exception.php
@@ -0,0 +1,36 @@
+<?php
+
+/*{{{*/
+/**
+ * ----------------------------------------------------------------------------
+ * Mumsys_Array2Xml_Exception
+ * for MUMSYS Library for Multi User Management System
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * ----------------------------------------------------------------------------
+ * @copyright (c) 2005 by Florian Blasel
+ * ----------------------------------------------------------------------------
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Array2Xml
+ * @version     1.3.1
+ * Created on 2005-01-15
+ * @filesource
+ * ----------------------------------------------------------------------------
+ */
+/*}}}*/
+
+
+/**
+ * Generic Exception class.
+ * 
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Array2Xml
+ */
+class Mumsys_Array2Xml_Exception extends Mumsys_Exception
+{
+
+}
