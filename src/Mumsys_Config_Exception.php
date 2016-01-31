f9cc913612bd7a9dd2ed5dd67cbdac76f2837495
flobee <flobee@r61>
2016-01-31 02:16:31 +0100

    Init Mumsys_Config_Exception as part for Configs handling

---
+++ src/Mumsys_Config_Exception.php
@@ -0,0 +1,36 @@
+<?php
+
+/*{{{*/
+/**
+ * ----------------------------------------------------------------------------
+ * Mumsys_Config_Exception
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * ----------------------------------------------------------------------------
+ * @copyright Copyright (c) 2009 by Florian Blasel for FloWorks Company
+ * ----------------------------------------------------------------------------
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Config
+ * @version     1.0.0
+ * Created: 2009-11-29
+ * @filesource
+ */
+/*}}}*/
+
+
+/**
+ * Generic Exception class which will be thown if no other exception will do
+ *
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Config
+ */
+class Mumsys_Config_Exception extends Mumsys_Exception
+{
+
+}
+
