e93c7720be2ad71d50036d023c2102fc5547817d
flobee <flobee@r61>
2016-02-05 17:49:46 +0100

    Init Mumsys_Context_Exception

---
+++ src/Mumsys_Context_Exception.php
@@ -0,0 +1,36 @@
+<?php
+
+/*{{{*/
+/**
+ * ----------------------------------------------------------------------------
+ * Mumsys_Context_Exception
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * ----------------------------------------------------------------------------
+ * @copyright Copyright (c) 2014 by Florian Blasel for FloWorks Company
+ * ----------------------------------------------------------------------------
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Context
+ * @version     1.0.0
+ * Created: 2014-01-06
+ * @filesource
+ */
+/*}}}*/
+
+
+/**
+ * Generic Exception class which will be thown if no other exception will do
+ *
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Context
+ */
+class Mumsys_Context_Exception extends Mumsys_Exception
+{
+
+}
+
