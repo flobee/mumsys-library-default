438aa94e3604fcc01c71321f8959b10e87bc96f8
flobee <flobee@r61>
2016-03-01 20:46:12 +0100

    Init Mumsys_Config tests

---
+++ tests/src/Mumsys_ConfigTest.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Test class for Mumsys_Config.
+ */
+class Mumsys_ConfigTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Mumsys_Config
+     */
+    protected $_object;
+    protected $_config;
+    protected $_context;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->_context = new Mumsys_Context();
+
+        $this->_config = require __DIR__ . '/../testconfig.php';
+
+        $this->_object = new Mumsys_Config($this->_context, $this->_config);
+    }
+
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        $this->_object = null;
+    }
+
+
+    public function test__construct()
+    {
+        $expected = MumsysTestHelper::getConfig();
+        $testvalues = $this->_object->getAll();
+        $this->assertEquals($expected, $testvalues);
+    }
+
+
+    public function testGet()
+    {
+        $key = 'debug';
+        $expected1 = true;
+        $testvalue1 = $this->_object->get($key, 123);
+        $expected2 = 123;
+        $testvalue2 = $this->_object->get('notExists', 123);
+
+        $this->assertEquals($expected1, $testvalue1[$key]);
+        $this->assertEquals($expected2, 123);
+    }
+
+
+    public function testGetExceptionB()
+    {
+        $actual = $this->_object->get(array('mumsys_title', 'B'));
+        $expected = array('mumsys_title' => null, 'B' => null);
+        $this->assertEquals($expected, $actual);
+    }
+
+
+    public function testGetAll()
+    {
+        $expected = $this->_object->getAll();
+        $testvalues = MumsysTestHelper::getConfig();
+        $this->assertEquals($expected, $testvalues);
+        $this->assertTrue( is_array($expected) );
+    }
+
+    public function testGetSubValues()
+    {
+        $actual = $this->_object->getSubValues(array('database'), array('type'));
+        $expected = array('type' => 'mysql:mysqli');
+    }
+
+    public function testRegister()
+    {
+        // simple
+        $this->_object->register('testkey', 'testvalue');
+        $expected1 = ['testkey' => 'testvalue'];
+        $testvalue1 = $this->_object->get('testkey');
+
+        $this->assertEquals($expected1, $testvalue1);
+
+        $this->setExpectedException(
+            'Mumsys_Config_Exception',
+            'Config key "testkey" already exists'
+        );
+        $this->_object->register('testkey', new stdClass());
+    }
+
+    public function testAdd()
+    {
+        $config = array('flo' => 'bee');
+        $this->_object->add($config);
+
+        $this->assertEquals($config, $this->_object->get('flo'));
+
+        $this->setExpectedException(
+            'Mumsys_Config_Exception', 'Config key "flo" already exists'
+        );
+        $this->_object->add($config);
+    }
+
+    public function testReplace()
+    {
+        $this->_object->register('testkey2', 'testvalue2');
+        $this->_object->replace('testkey2', 'testvalue3');
+
+        $this->assertEquals(array('testkey2' => 'testvalue3'), $this->_object->get('testkey2'));
+    }
+
+    public function testLoad()
+    {
+        $this->setExpectedException('Mumsys_Config_Exception');
+        $this->_object->load('mumsys2');
+    }
+
+}
