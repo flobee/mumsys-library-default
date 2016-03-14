3e414d0cda80b93d5d7356f8d7975654322757af
flobee <flobee@r61>
2016-03-14 18:07:21 +0100

    Init Mumsys_Mvc_Display_Control_Stdout

--- src/Mumsys_Mvc_Display_Control_Stdout.php
+++ src/Mumsys_Mvc_Display_Control_Stdout.php
@@ -3,18 +3,18 @@
 /*{{{*/
 /**
  * ----------------------------------------------------------------------------
- * Mumsys_Mvc_Display_Control_Text
+ * Mumsys_Mvc_Display_Control_Stdout
  * for MUMSYS Library for Multi User Management System (MUMSYS)
  * ----------------------------------------------------------------------------
  * @author Florian Blasel <flobee.code@gmail.com>
- * @copyright Copyright (c) 2006 by Florian Blasel for FloWorks Company
+ * @copyright Copyright (c) 2015 by Florian Blasel for FloWorks Company
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
  * ----------------------------------------------------------------------------
  * @category    Mumsys
  * @package     Mumsys_Library
  * @subpackage  Mumsys_Mvc
  * @version     1.0.0
- * Created: 2006-12-01
+ * Created: 2015-12-01
  * @filesource
  */
 /*}}}*/
@@ -32,6 +32,7 @@
  */
 class Mumsys_Mvc_Display_Control_Stdout
     extends Mumsys_Mvc_Display_Control_Abstract
+    implements Mumsys_Mvc_Display_Control_Interface
 {
     /**
      * Version ID information
@@ -45,16 +46,11 @@ class Mumsys_Mvc_Display_Control_Stdout
     private $_context;
 
     /**
-     * Name of the template/ theme to be used
-     * Its the path for the templates. Default: "default"
-     * @var string
-     */
-    protected $_template;
-
-    /**
-     * Initialize the display controller text version.
+     * Initialize the display controller text/ stdout version.
      *
-     * @param Mumsys_Context $context Context oject
+     * @todo pagetitle by given program infomations
+     *
+     * @param Mumsys_Context $context Context object
      * @param array $options array with options
      * [program] optional "program" to call, if empty the default program will be used
      * [controller] optional controller to use
@@ -63,13 +59,6 @@ class Mumsys_Mvc_Display_Control_Stdout
     public function __construct( Mumsys_Context $context, array $options = array() )
     {
         $this->_context = $context;
-        $this->_template = 'default';
-
-        /**
-         * @todo implement permissions pagetitle by given program infomations like:
-         * if ( empty($opts['pagetitle']) ) {
-         * $this->_pagetitle .= ' : ' . $modinfo[mod]['submodules'][submod]['name'];
-         */
     }
 
 }
