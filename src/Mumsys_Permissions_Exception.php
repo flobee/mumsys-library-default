b50f6d148d541236c78db90486e759a7afedf91d
flobee <flobee@r61>
2016-03-23 22:28:45 +0100

    Init Mumsys_Permissions_Exception from svn repo

---
+++ src/Mumsys_Permissions_Exception.php
@@ -0,0 +1,31 @@
+<?php
+
+/*{{{*/
+/**
+ * Mumsys_Permissions_Exception
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * @copyright Copyright (c) 2015 by Florian Blasel for FloWorks Company
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Permissions
+ * @version     3.0.0
+ * Created: 2015-08-11
+ * @filesource
+ */
+/*}}}*/
+
+/**
+ * Generic exception class Mumsys_Permissions_Exception
+ *
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Permissions
+ */
+class Mumsys_Permissions_Exception extends Mumsys_Exception
+{
+}
+
