46cfccda160b1a359ed091b67ed41f43b27c8fac
flobee <flobee@r61>
2016-03-23 22:30:02 +0100

    Init Mumsys_Permissions_Abstract from svn repo

---
+++ src/Mumsys_Permissions_Abstract.php
@@ -0,0 +1,39 @@
+<?php
+
+/*{{{*/
+/**
+ * Mumsys_Permissions_Abstract
+ * for MUMSYS (Multi User Management System)
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * @copyright Copyright (c) 2016 by Florian Blasel for FloWorks Company
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Permissions
+ * @version     3.1.0
+ * Created: 2016-01-19
+ * @filesource
+ */
+/*}}}*/
+
+/**
+ * Class to deal with the permissions (acl)
+ *
+ * @category Mumsys
+ * @package Mumsys_Library
+ * @subpackage Mumsys_Permissions
+ */
+abstract class Mumsys_Permissions_Abstract
+    extends Mumsys_Abstract
+    implements Mumsys_Permissions_Interface
+{
+    /**
+     * Version ID information
+     */
+    const VERSION = '3.1.0';
+
+    
+
+}
