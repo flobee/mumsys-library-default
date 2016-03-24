914283488a13ff68659f2f0ec4b218a38ca53240
flobee <flobee@r61>
2016-03-24 21:59:42 +0100

    Init Mumsys_Request_Shell from svn repo

---
+++ src/Mumsys_Request_Shell.php
@@ -0,0 +1,62 @@
+<?php
+
+/* {{{ */
+/**
+ * Mumsys_Request_Shell
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * @copyright Copyright (c) 2016 by Florian Blasel for FloWorks Company
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Request
+ * @filesource
+ */
+/* }}} */
+
+
+/**
+ * Request class to get input parameters from shell input
+ *
+ * @category    Mumsys_
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Request
+ */
+class Mumsys_Request_Shell
+    extends Mumsys_Request_Abstract
+    implements Mumsys_Request_Interface
+{
+    /**
+     * Version ID information
+     */
+    const VERSION = '1.0.0';
+
+
+    /**
+     * Initialise the request class using _GET and _POST arrays.
+     *
+     * @param array $options Optional initial options e.g.:
+     * 'programKey','controllerKey', 'actionKey',
+     */
+    public function __construct( array $options = array() )
+    {
+        if (isset($options['programKey'])) {
+            $this->setProgramKey($options['programKey']);
+        }
+
+        if (isset($options['controllerKey'])) {
+            $this->setControllerKey($options['controllerKey']);
+        }
+
+        if (isset($options['actionKey'])) {
+            $this->setActionKey($options['actionKey']);
+        }
+
+        if (isset($_SERVER['argv']) && is_array($_SERVER['argv'])) {
+            $this->_input += $_SERVER['argv'];
+        }
+    }
+
+}
