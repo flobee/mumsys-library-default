3cae9434427d4dab5a5f670bd14e976760c4c75a
flobee <flobee@r61>
2016-03-25 21:35:46 +0100

    Init Mumsys_I18n_Exception from svn repo

---
+++ src/Mumsys_I18n_Exception.php
@@ -0,0 +1,31 @@
+<?php
+
+/*{{{*/
+/**
+ * Mumsys_I18n_Exception
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * @copyright Copyright (c) 2013 by Florian Blasel for FloWorks Company
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_I18n
+ * @version 1.0.0
+ * Created on 2013-12-17
+ */
+/*}}}*/
+
+
+/**
+ * Generic Exception class for I18n interface
+ *
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_I18n
+ */
+class Mumsys_I18n_Exception extends Mumsys_Exception
+{
+
+}
