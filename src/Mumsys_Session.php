e24839737a1e934de7f3f65ca5045263c3a4adea
flobee <flobee@r61>
2016-04-14 21:55:25 +0200

    Improves the session handling; Adds application key to use the session in different applications which may have the same sessionID Improves tests: 100%

--- src/Mumsys_Session.php
+++ src/Mumsys_Session.php
@@ -2,14 +2,11 @@
 
 /*{{{*/
 /**
- * ----------------------------------------------------------------------------
  * Mumsys_Session
  * for MUMSYS Library for Multi User Management System (MUMSYS)
  * ----------------------------------------------------------------------------
- * @author Florian Blasel <flobee.code@gmail.com>
- * ----------------------------------------------------------------------------
  * @copyright Copyright (c) 2005 by Florian Blasel for FloWorks Company
- * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
  * ----------------------------------------------------------------------------
  * @category    Mumsys
@@ -34,7 +31,7 @@ class Mumsys_Session extends Mumsys_Abstract
     /**
      * Version ID information
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     /**
      * Representation of the session befor it will be set to $_SESSION
@@ -42,13 +39,25 @@ class Mumsys_Session extends Mumsys_Abstract
      */
     private $_records;
 
+    /**
+     * Session ID.
+     * @var string
+     */
     private $_id;
+
+    /**
+     * Application key the session belongs to.
+     * @var string
+     */
+    private $_appKey;
 
 
     /**
      * Initialize the session object.
+     *
+     * @param string $appkey Application key the session belongs to.
      */
-    public function __construct()
+    public function __construct( $appkey = 'mumsys' )
     {
         /**
          * session_cache_limiter('private');
@@ -56,19 +65,20 @@ class Mumsys_Session extends Mumsys_Abstract
          * session_cache_expire(180);
          * echo $cache_expire = session_cache_expire();
          */
-        if (isset($_SESSION)) {
-            $this->_id = session_id();
+        @session_start();
+        $this->_id = session_id();
+        $this->_appKey = $appkey;
+
+        if ($_SESSION) {
             $this->_records = & $_SESSION;
         } else {
-            @session_start();
-            $this->_id = session_id();
-            $this->_records[$this->_id] = array();
+            $this->_records[$this->_id][$appkey] = array();
         }
     }
 
 
     /**
-     * Stores session informations to the session
+     * Stores session informations to the session on exit.
      */
     public function __destruct()
     {
@@ -85,24 +95,24 @@ class Mumsys_Session extends Mumsys_Abstract
      */
     public function get( $key, $default = null )
     {
-        if (isset($this->_records[$this->_id][$key])) {
-            return $this->_records[$this->_id][$key];
+        if (isset($this->_records[$this->_id][$this->_appKey][$key])) {
+            return $this->_records[$this->_id][$this->_appKey][$key];
         }
 
         return $default;
     }
 
     /**
-     * Returns the current session dat based on the current session id.
+     * Returns the current session data based on the current session id and
+     * application key.
      *
      * Note: This is befor it will be available in $_SESSION.
      *
-     * @param string $key value of the key to return to
      * @return mixed Stored value
      */
     public function getCurrent()
     {
-        return $this->_records[$this->_id];
+        return $this->_records[$this->_id][$this->_appKey];
     }
 
     /**
@@ -133,12 +143,12 @@ class Mumsys_Session extends Mumsys_Abstract
     {
         $key = (string)$key;
 
-        if (array_key_exists($key, $this->_records[$this->_id])) {
+        if (array_key_exists($key, $this->_records[$this->_id][$this->_appKey])) {
             $message = sprintf('Session key "%1$s" exists', $key);
             throw new Mumsys_Session_Exception($message);
         }
 
-        $this->_records[$this->_id][$key] = $value;
+        $this->_records[$this->_id][$this->_appKey][$key] = $value;
     }
 
 
@@ -150,7 +160,7 @@ class Mumsys_Session extends Mumsys_Abstract
      */
     public function replace( $key, $value = null )
     {
-        $this->_records[$this->_id][$key] = $value;
+        $this->_records[$this->_id][$this->_appKey][$key] = $value;
     }
 
 
@@ -166,7 +176,7 @@ class Mumsys_Session extends Mumsys_Abstract
 
 
     /**
-     * Clears and unsets the current session
+     * Clears and unsets the current session at all.
      */
     public function clear()
     {
