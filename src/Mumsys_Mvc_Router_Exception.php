0e556be2be9a6bfb68da08faf8cdeb4f58b4e901
flobee <flobee@r61>
2016-04-25 22:57:01 +0200

    init Mumsys_Mvc_Router_Exception from svn repro

---
+++ src/Mumsys_Mvc_Router_Exception.php
@@ -0,0 +1,35 @@
+<?php
+
+
+/* {{{ */
+/**
+ * ----------------------------------------------------------------------------
+ * Mumsys_Mvc_Router_Exception
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * @copyright Copyright (c) 2016 by Florian Blasel for FloWorks Company
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * ----------------------------------------------------------------------------
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Mvc
+ * @version     1.0.0
+ * Created: 2016-01-30
+ * @filesource
+ */
+/* }}} */
+
+
+/**
+ * Generic Exception class which will be thown if no other exception will do
+ *
+ * @category    Mumsys
+ * @package     Mumsys_Library
+ * @subpackage  Mumsys_Mvc
+ */
+class Mumsys_Mvc_Router_Exception
+    extends Mumsys_Mvc_Exception
+{
+
+}
