f3e6ead4cef8f69f290298cc9152549d976f1681
flobee <flobee@r61>
2016-05-17 18:54:05 +0200

    Adds missing methodes Improvements in coding style Updates phpdoc

--- src/Mumsys_Session_Interface.php
+++ src/Mumsys_Session_Interface.php
@@ -1,23 +1,21 @@
 <?php
 
-/*{{{*/
+/* {{{ */
 /**
- * ----------------------------------------------------------------------------
  * Mumsys_Session_Interface
  * for MUMSYS Library for Multi User Management System (MUMSYS)
  * ----------------------------------------------------------------------------
- * @author Florian Blasel <flobee.code@gmail.com>
- * @copyright Copyright (c) 2005 by Florian Blasel for FloWorks Company
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2005 by Florian Blasel for FloWorks Company
+ * @author Florian Blasel <flobee.code@gmail.com>
  * ----------------------------------------------------------------------------
  * @category    Mumsys
  * @package     Mumsys_Library
  * @subpackage  Mumsys_Session
- * @version     1.0.0
- * Created: 2016-03-19
- * @filesource
+ * @version     1.1.0
+ * Created: 2005-01-01, new in 2016-03-19
  */
-/*}}}*/
+/* }}} */
 
 
 /**
@@ -28,8 +26,20 @@
  * @subpackage  Mumsys_Session
  */
 interface Mumsys_Session_Interface
-    extends Mumsys_GetterSetter_Interface
+extends Mumsys_GetterSetter_Interface
 {
+
+
+    /**
+     * Returns the value of given key.
+     *
+     * @param string $key value of the key to return to
+     * @param scalar $default
+     * @return mixed Stored value or $default if key was not set.
+     */
+    public function get( $key, $default = null );
+
+
     /**
      * Returns the current session data based on the current session id.
      *
@@ -39,6 +49,7 @@ interface Mumsys_Session_Interface
      * @return mixed Stored value
      */
     public function getCurrent();
+
 
     /**
      * Returns the complete active session data.
@@ -51,12 +62,46 @@ interface Mumsys_Session_Interface
      */
     public function getAll();
 
+
+    /**
+     * Register a new session key.
+     *
+     * Its like register a domain for the application. This is the failsave
+     * setter to add new values to the session.
+     *
+     * @param string $key Name of the key to register.
+     * @param mixed $value optional; Value to be stored
+     * @throws Mumsys_Session_Exception Throws exception if key already exists
+     */
+    public function register( $key, $value = null );
+
+
+    /**
+     * Replace/sets the value for the given key.
+     *
+     * @param string $key Identifier/ session key to set
+     * @param mixed $value Value to be stored
+     */
+    public function replace( $key, $value = null );
+
+
+    /**
+     * Removes registered entry.
+     *
+     * @param string $key Key which was set
+     *
+     * @return boolean True on success or false if the key wasn't set before
+     */
+    public function remove( $key );
+
+
     /**
      * Returns the session ID.
      *
      * @return string
      */
     public function getID();
+
 
     /**
      * Clears and unsets the current session
