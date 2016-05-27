9fcab32d6b0e5ebf6621c3ba8298ae8702530b1d
flobee <flobee@r61>
2016-05-27 23:55:38 +0200

    Improves CS

--- src/Mumsys_FileSystem_Common_Abstract.php
+++ src/Mumsys_FileSystem_Common_Abstract.php
@@ -1,24 +1,20 @@
 <?php
 
-/*{{{*/
+/* {{{ */
 /**
- * ----------------------------------------------------------------------------
  * Mumsys_FileSystem_Common_Abstract
  * for MUMSYS Library for Multi User Management System (MUMSYS)
  * ----------------------------------------------------------------------------
- * @author Florian Blasel <flobee.code@gmail.com>
- * ----------------------------------------------------------------------------
- * @copyright Copyright (c) 2007 by Florian Blasel for FloWorks Company
- * ----------------------------------------------------------------------------
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2007 by Florian Blasel for FloWorks Company
+ * @author Florian Blasel <flobee.code@gmail.com>
  * ----------------------------------------------------------------------------
  * @category        Mumsys
  * @package         Mumsys_Library
  * @subpackage      Mumsys_FileSystem
- * @version         3.1.0
  * 0.1 - Created on 2009-11-27
  */
-/*}}}*/
+/* }}} */
 
 
 /**
@@ -44,10 +40,10 @@ abstract class Mumsys_FileSystem_Common_Abstract
      * @param string $file Filename or file location
      * @return string string of the extension or '' (empty string)
      */
-    public static function extGet($file)
+    public static function extGet( $file )
     {
         $ext = strrchr(basename($file), '.');
-        if ($ext) {
+        if ( $ext ) {
             $ext = substr($ext, 1);
         } else {
             $ext = '';
@@ -65,13 +61,12 @@ abstract class Mumsys_FileSystem_Common_Abstract
     public static function nameGet( $file = '' )
     {
         $pos = strrpos($file, '.');
-        if ($pos === false) {
+        if ( $pos === false ) {
             return basename($file);
         } else {
             return substr(basename($file), 0, $pos);
         }
     }
-
 
     /**
      * Get extension of current file
