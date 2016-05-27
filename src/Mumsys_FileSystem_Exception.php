f1602038ac099222b726e6c1cba90a30f71e83fb
flobee <flobee@r61>
2016-05-27 23:51:55 +0200

    Improves CS

--- src/Mumsys_FileSystem_Exception.php
+++ src/Mumsys_FileSystem_Exception.php
@@ -1,24 +1,21 @@
 <?php
-/*{{{*/
+
+/* {{{ */
 /**
- * ----------------------------------------------------------------------------
  * Mumsys_FileSystem_Exception
  * for MUMSYS Library for Multi User Management System (MUMSYS)
  * ----------------------------------------------------------------------------
- * @author Florian Blasel <flobee.code@gmail.com>
- * ----------------------------------------------------------------------------
- * @copyright Copyright (c) 2015 by Florian Blasel
- * ----------------------------------------------------------------------------
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2015 by Florian Blasel
+ * @author Florian Blasel <flobee.code@gmail.com>
  * ----------------------------------------------------------------------------
  * @category    Mumsys
  * @package     Mumsys_Library
  * @subpackage  Mumsys_FileSystem
  * @version     3.0.0
  * 0.1 - Created on 2015/03
- * ----------------------------------------------------------------------------
  */
-/*}}}*/
+/* }}} */
 
 
 /**
@@ -28,7 +25,8 @@
  * @package     Mumsys_Library
  * @subpackage  Mumsys_FileSystem
  */
-class Mumsys_FileSystem_Exception extends Mumsys_Exception
+class Mumsys_FileSystem_Exception
+    extends Mumsys_Exception
 {
 
 }
