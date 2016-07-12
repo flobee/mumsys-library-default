f38a9ac2488636ea90a3ac4f44e76d14edc072cb
flobee <flobee@r61>
2016-07-12 19:52:49 +0200

    Init Mumsys_Variable_Item_Abstract Tests

---
+++ tests/src/Mumsys_Variable_Item_AbstractTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Mumsys_Variable_Item_Abstract Test
+ */
+class Mumsys_Variable_Item_AbstractTest
+    extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Mumsys_Variable_Item_Default
+     */
+    protected $_object;
+
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->_config = array(
+            'name' => 'somevariable',
+            'value' => 'init value'
+        );
+        $this->_object = new Mumsys_Variable_Item_Default($this->_config);
+    }
+
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        $this->_object = NULL;
+    }
+
+
+    /**
+     * @covers Mumsys_Variable_Item_Abstract::getName
+     * @covers Mumsys_Variable_Item_Abstract::setName
+     */
+    public function testGetSetName()
+    {
+        $this->_object->setName('somevariable2');
+        $this->assertEquals('somevariable2', $this->_object->getName());
+    }
+
+
+    /**
+     * @covers Mumsys_Variable_Item_Abstract::getValue
+     * @covers Mumsys_Variable_Item_Abstract::setValue
+     */
+    public function testGetSetValue()
+    {
+        $this->_object->setValue('somevariable2');
+        $this->assertEquals('somevariable2', $this->_object->getValue());
+    }
+
+
+    /**
+     * @covers Mumsys_Variable_Item_Abstract::setErrorMessage
+     * @covers Mumsys_Variable_Item_Abstract::getErrorMessages
+     */
+    public function testGetSetErrorMessages()
+    {
+        $expected = array('testKey' => 'testMessage');
+        $this->_object->setErrorMessage('testKey', 'testMessage');
+        $this->assertEquals($expected, $this->_object->getErrorMessages());
+    }
+
+}
