b31794bdd2a9f880e0f2a8760570b053844ad729
flobee <flobee@r61>
2016-07-26 22:47:11 +0200

    Adds/init copy from svn html table xml abstract,exception + tests

---
+++ src/Mumsys_Html_Exception.php
@@ -0,0 +1,31 @@
+<?php
+
+/* {{{ */
+/**
+ * Mumsys_Html_Exception
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2016 by Florian Blasel for FloWorks Company
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * ----------------------------------------------------------------------------
+ * @category Mumsys
+ * @package Mumsys_Library
+ * @subpackage Mumsys_Html
+ * @version 0.1 - Created on 2010-12-08
+ */
+/* }}} */
+
+
+/**
+ * Generic exception class.
+ *
+ * @category Mumsys
+ * @package Mumsys_Library
+ * @subpackage Mumsys_Html
+ */
+class Mumsys_Html_Exception
+    extends Mumsys_Exception
+{
+
+}
