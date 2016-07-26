db22faf51fedcc46bcb1d5fdb4f37d6652efb068
flobee <flobee@r61>
2016-07-26 22:58:06 +0200

    Adds mumsys xml exception

---
+++ src/Mumsys_Xml_Exception.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * {{{ Mumsys_Xml_Exception
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ * ----------------------------------------------------------------------------
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2016 by Florian Blasel for FloWorks Company
+ * @author Florian Blasel <flobee.code@gmail.com>
+ * ----------------------------------------------------------------------------
+ * @category Mumsys
+ * @package Mumsys_Library
+ * @subpackage Mumsys_Xml
+ }}} */
+
+
+/**
+ * Generic exception class.
+ *
+ * @category Mumsys
+ * @package Mumsys_Library
+ * @subpackage Mumsys_Xml
+ */
+class Mumsys_Xml_Exception
+    extends Mumsys_Exception
+{
+
+}
