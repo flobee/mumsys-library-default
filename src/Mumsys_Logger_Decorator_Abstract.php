f0ddcfad4d96d332c5f74c58803292904650f119
flobee <flobee@r61>
2016-11-18 22:02:42 +0100

    Adds common methodes to decorator abstract class

--- src/Mumsys_Logger_Decorator_Abstract.php
+++ src/Mumsys_Logger_Decorator_Abstract.php
@@ -29,6 +29,11 @@ abstract class Mumsys_Logger_Decorator_Abstract
      */
     const VERSION = '3.0.0';
 
+    /**
+     * Interface to decorate.
+     * @var Mumsys_Logger_Interface
+     */
+    private $_object;
 
     /**
      * Initialize the decorated logger object
@@ -50,6 +55,19 @@ abstract class Mumsys_Logger_Decorator_Abstract
     }
 
 
+    /**
+     * Get the name of a log level.
+     *
+     * @param integer $level Nuber of the Log level
+     *
+     * @return string Returns the string of the errorlevel
+     */
+    public function getLevelName( $level )
+    {
+        return $this->_object->getLevelName($level);
+    }
+
+    
     /**
      * Create a log entry by a given message and log level.
      *
@@ -73,9 +91,132 @@ abstract class Mumsys_Logger_Decorator_Abstract
         return $this->_object->log($input, $level);
     }
 
+    /**
+     * Create an emergency log entry.
+     * Alias method for log()
+     *
+     * @param string $message Message to be logged
+     *
+     * @return string Returns the formated log message string
+     */
+    public function emerg( $message )
+    {
+        return $this->log($message, Mumsys_Logger_Abstract::EMERG);
+    }
+
 
     /**
-     * Returns the decorated object.
+     * Create an alert log entry.
+     * Alias method for log()
+     *
+     * @param string $message Message to be logged
+     *
+     * @return string Returns the formated log message string
+     */
+    public function alert( $message )
+    {
+        return $this->log($message, Mumsys_Logger_Abstract::ALERT);
+    }
+
+
+    /**
+     * Create an critical log entry.
+     * Alias method for log()
+     *
+     * @param string $message Message to be logged
+     *
+     * @return string Returns the formated log message string
+     */
+    public function crit( $message )
+    {
+        return $this->log($message, Mumsys_Logger_Abstract::CRIT);
+    }
+
+
+    /**
+     * Create an error log entry.
+     * Alias method for log()
+     *
+     * @param string $message Message to be logged
+     *
+     * @return string Returns the formated log message string
+     */
+    public function err( $message )
+    {
+        return $this->log($message, Mumsys_Logger_Abstract::ERR);
+    }
+
+
+    /**
+     * Create a warning log entry.
+     * Alias method for log()
+     *
+     * @param string $message Message to be logged
+     *
+     * @return string Returns the formated log message string
+     */
+    public function warn( $message )
+    {
+        return $this->log($message, Mumsys_Logger_Abstract::WARN);
+    }
+
+
+    /**
+     * Create a notice log entry.
+     * Alias method for log()
+     *
+     * @param string $message Message to be logged
+     *
+     * @return string Returns the formated log message string
+     */
+    public function notice( $message )
+    {
+        return $this->log($message, Mumsys_Logger_Abstract::NOTICE);
+    }
+
+
+    /**
+     * Create an information log entry.
+     * Alias method for log()
+     *
+     * @param string $message Message to be logged
+     *
+     * @return string Returns the formated log message string
+     */
+    public function info( $message )
+    {
+        return $this->log($message, Mumsys_Logger_Abstract::INFO);
+    }
+
+
+    /**
+     * Create a debug log entry.
+     * Alias method for log()
+     *
+     * @param string $message Message to be logged
+     *
+     * @return string Returns the formated log message string
+     */
+    public function debug( $message )
+    {
+        return $this->log($message, Mumsys_Logger_Abstract::DEBUG);
+    }
+
+    /**
+     * Checks if a loglevel is registered or not
+     *
+     * @param integer $level Log level to be checked
+     *
+     * @return boolean Returns true for OK otherwise false
+     */
+    public function checkLevel( $level = 0 )
+    {
+        return $this->_object->checkLevel($level);
+    }
+
+
+    /**
+     * Returns the decorated/ the base object.
      *
      * @return Mumsys_Logger_Interface Config object
      */
