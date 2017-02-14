fd28a2fc5910ec5274744e0a1b560935074ba91a
flobee <flobee@r61>
2017-02-14 19:28:40 +0100

    Re-Add old context item for fallbacks

---
+++ src/Mumsys_Context.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Mumsys_Context
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ *
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2014 by Florian Blasel for FloWorks Company
+ * @author Florian Blasel <flobee.code@gmail.com>
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Context
+ */
+
+
+/**
+ * @deprecated since version 3.0.0 Use Mumsys_Context_Item
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Context
+ */
+class Mumsys_Context
+    extends Mumsys_Context_Item
+    implements Mumsys_Context_Interface
+{
+    /**
+     * Version ID information
+     */
+    const VERSION = '2.0.0';
+
+}
