80b71c22ee8ccce00ef0442215b311622e2ae916
flobee <flobee@r61>
2017-02-23 16:52:44 +0100

    init phpt upload tests

---
+++ tests/src/Mumsys_Assets_Upload_PostRawTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Mumsys_Assets_Upload_PostRawTest extends PHPUnit_Extensions_PhptTestCase
+{
+    public function __construct()
+    {
+        parent::__construct(__DIR__ . '/../phpt/Mumsys_Assets_Upload_PostRawTest.phpt');
+    }
+
+}
+
+
+
+
