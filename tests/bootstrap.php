9e14cb5f2f403c37f8ac4d04a08e9b099ca15749
flobee <flobee@r61>
2017-09-20 23:43:56 +0200

    Improves CS

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@
  * Bootstrap for Mumsys Library tests
  */
 
-if (in_array('root',$_SERVER)) {
+if ( in_array('root', $_SERVER) ) {
     exit('Something belongs to root. Use a different user! Security exit.' . PHP_EOL);
 }
 
@@ -12,9 +12,9 @@ ini_set('include_path', '../src/' . PATH_SEPARATOR . get_include_path());
 
 date_default_timezone_set('Europe/Berlin');
 
-setlocale(LC_ALL, 'POSIX');// "C" style
+setlocale(LC_ALL, 'POSIX'); // "C" style
 
-require_once  __DIR__ . '/../src/Mumsys_Loader.php';
+require_once __DIR__ . '/../src/Mumsys_Loader.php';
 spl_autoload_register(array('Mumsys_Loader', 'autoload'));
 
 require_once __DIR__ . '/testconstants.php';
