b5590f662ef52029152dd0f4d392b99314c378ea
flobee <flobee@debian>
2017-11-30 20:03:16 +0100

    Init service domain

---
+++ src/Mumsys_Service_Exception.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mumsys_Sevice_Exception
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ *
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2015 by Florian Blasel for FloWorks Company
+ * @author Florian Blasel <flobee.code@gmail.com>
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Service
+ * @version     1.0.0
+ * Created: 2016-11-30
+ */
+
+/**
+ * Generic exception class.
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Service
+ */
+class Mumsys_Sevice_Exception extends Mumsys_Exception
+{
+
+}
+
