c1c488e683ec1fa3612c947bf41e84b97a81de64
flobee <flobee@debian>
2017-12-10 12:18:28 +0100

    Adds tests to be 100% CC

--- tests/src/Mumsys_Service_Spss_ReaderTest.php
+++ tests/src/Mumsys_Service_Spss_ReaderTest.php
@@ -1,6 +1,22 @@
 <?php
 
 /**
+ * Mumsys_Service_Spss_ReaderTest
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ *
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2015 by Florian Blasel for FloWorks Company
+ * @author Florian Blasel <flobee.code@gmail.com>
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Service
+ * @version     1.0.0
+ * Created: 2017-11-30
+ */
+
+
+/**
  * Mumsys_Service_Spss_Reader Test
  */
 class Mumsys_Service_Spss_ReaderTest
@@ -18,10 +34,12 @@ class Mumsys_Service_Spss_ReaderTest
      */
     protected function setUp()
     {
+        $this->_version = '1.0.0';
+
         $spssFile = __DIR__ . '/../testfiles/Service/Spss/pspp.sav';
         $parser = \SPSS\Sav\Reader::fromString( file_get_contents( $spssFile ) );
 
-        $this->_object = new Mumsys_Service_Spss_Reader($parser);
+        $this->_object = new Mumsys_Service_Spss_Reader( $parser );
     }
 
 
@@ -37,6 +55,7 @@ class Mumsys_Service_Spss_ReaderTest
 
     /**
      * @covers Mumsys_Service_Spss_Reader::getVariableItems
+     * @covers Mumsys_Service_Spss_Reader::_checkVariableNameExists
      */
     public function testGetVariableItems()
     {
@@ -60,63 +79,183 @@ class Mumsys_Service_Spss_ReaderTest
             3 => 0,
         );
 
-        $expected = array( $obj );
+        $expected = array($obj);
+        $actual = $this->_object->getVariableItems( $list );
 
-        $actual = $this->_object->getVariableItems($list);
-
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals( $expected, $actual );
     }
 
 
     /**
      * @covers Mumsys_Service_Spss_Reader::getVariableMapByLabels
-     * @todo   Implement testGetVariableMapByLabels().
      */
     public function testGetVariableMapByLabels()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
+        $labels = array(
+            'v1int', 'v2float', 'v3string'
         );
+        $actual = $this->_object->getVariableMapByLabels( $labels );
+        $expected = array(
+            'V1INT' => 'v1int',
+            'V2FLOAT' => 'v2float',
+            'V3STRING' => 'v3string'
+        );
+
+        $this->assertEquals( $expected, $actual );
     }
 
 
     /**
      * @covers Mumsys_Service_Spss_Reader::getVariableMapByKeys
-     * @todo   Implement testGetVariableMapByKeys().
      */
     public function testGetVariableMapByKeys()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
+        $keys = array(
+            'V1INT', 'V2FLOAT', 'V3STRING'
         );
+        $actual = $this->_object->getVariableMapByKeys( $keys );
+        $expected = array(
+            'V1INT' => 'v1int',
+            'V2FLOAT' => 'v2float',
+            'V3STRING' => 'v3string'
+        );
+
+        $this->assertEquals( $expected, $actual );
     }
 
 
     /**
      * @covers Mumsys_Service_Spss_Reader::getVariableMapByRegex
-     * @todo   Implement testGetVariableMapByRegex().
      */
     public function testGetVariableMapByRegex()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
+        $regex = array(
+            '/(v)/i'
         );
+        $actual1 = $this->_object->getVariableMapByRegex( $regex );
+        $actual2 = $this->_object->getVariableMapByRegex();
+        $expected1 = array(
+            'V1INT' => 'v1int',
+            'V2FLOAT' => 'v2float',
+            'V3STRING' => 'v3string'
+        );
+
+        $this->assertEquals( $expected1, $actual1 );
+        $this->assertEquals( $expected1, $actual2 );
+
+        ini_set( "error_reporting", 0 );
+        $this->expectException( 'Mumsys_Service_Spss_Exception' );
+        $this->expectExceptionMessage( 'Regex error' );
+        $this->_object->getVariableMapByRegex( array('/f#') );
+        ini_set( "error_reporting", -1 );
     }
 
 
     /**
      * @covers Mumsys_Service_Spss_Reader::getVariableMap
-     * @todo   Implement testGetVariableMap().
      */
     public function testGetVariableMap()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
+        $actual1 = $this->_object->getVariableMap();
+        $expected = array(
+            'V1INT' => 'v1int',
+            'V2FLOAT' => 'v2float',
+            'V3STRING' => 'v3string'
         );
+
+        $actual2 = $this->_object->getVariableMap();
+
+        $this->assertEquals( $expected, $actual1 );
+        $this->assertEquals( $expected, $actual2 );
     }
 
+
+    /**
+     * @covers Mumsys_Service_Spss_Reader::getData
+     */
+    public function testGetData()
+    {
+        $actual = $this->_object->getData();
+        $expected = array(
+            array(1, 1.23, 'ab'),
+            array(2.0, 2.3399999999999, 'bc'),
+            array(3, 3.45, 'cd')
+        );
+
+        $this->assertEquals( $expected, $actual );
+    }
+
+
+    /**
+     * @covers Mumsys_Service_Spss_Reader::getDocumentInfo
+     */
+    public function testGetDocumentInfo()
+    {
+        $actual = $this->_object->getDocumentInfo();
+        $expected = array(
+            'comment to file                                                                 ',
+            'Added by pspp:  (17 Nov 2017 eingegeben)                                        ',
+            '                                                                                ',
+            '   (17 Nov 2017 eingegeben)                                                     '
+        );
+
+        $this->assertEquals( $expected, $actual );
+    }
+
+    /**
+     * @covers Mumsys_Service_Spss_Reader::getEncoding
+     */
+    public function testGetEncoding()
+    {
+        $actual1 = $this->_object->getEncoding();
+        $actual2 = $this->_object->getEncoding();
+        $expected = 'UTF-8';
+
+        $this->assertEquals( $expected, $actual1 );
+        $this->assertEquals( $expected, $actual2 );
+    }
+
+
+    /**
+     * @covers Mumsys_Service_Spss_Reader::getVersionOfSource
+     */
+    public function testGetVersionOfSource()
+    {
+        $actual1 = $this->_object->getVersionOfSource();
+        $actual2 = $this->_object->getVersionOfSource();
+        $expected = array(0,10,2);
+
+        $this->assertEquals( $expected, $actual1 );
+        $this->assertEquals( $expected, $actual2 );
+    }
+
+
+    /**
+     * @covers Mumsys_Service_Spss_Reader::getFloatingPointInfo
+     */
+    public function testGetFloatingPointInfo()
+    {
+        $actual1 = $this->_object->getFloatingPointInfo();
+        $actual2 = $this->_object->getFloatingPointInfo();
+        $expected = array(
+            'sysmis' => -1.7976931348623157E+308,
+            'highest' => 1.7976931348623157E+308,
+            'lowest' => -1.7976931348623155E+308,
+        );
+
+        $this->assertEquals( $expected, $actual1 );
+        $this->assertEquals( $expected, $actual2 );
+    }
+
+
+
+    /**
+     * @c o v e r s Mumsys_Service_Spss_Reader::VERSION
+     */
+    public function testVersion()
+    {
+        $actual = Mumsys_Service_Spss_Reader::VERSION;
+
+        $this->assertEquals($this->_version, $actual);
+    }
 }
