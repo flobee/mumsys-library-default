4cb439b971ab3aae0928a37d96176cb787fcc060
flobee <flobee@debian>
2017-12-15 22:27:53 +0100

    Improves CS, phpdoc

--- src/Mumsys_Xml_Abstract.php
+++ src/Mumsys_Xml_Abstract.php
@@ -1,25 +1,25 @@
 <?php
 
 /**
- * {{{ Mumsys_Xml_Abstract
+ * Mumsys_Xml_Abstract
  * for MUMSYS Library for Multi User Management System (MUMSYS)
- * ----------------------------------------------------------------------------
+ *
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
  * @copyright Copyright (c) 2016 by Florian Blasel for FloWorks Company
  * @author Florian Blasel <flobee.code@gmail.com>
- * ----------------------------------------------------------------------------
- * @category Mumsys
- * @package Mumsys_Library
- * @subpackage Mumsys_Xml
- }}} */
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Xml
+ */
 
 
 /**
  * Class for xml code creation, validation and filtering
  *
- * @category Mumsys
- * @package Mumsys_Library
- * @subpackage Mumsys_Xml
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Xml
  */
 abstract class Mumsys_Xml_Abstract
     extends Mumsys_Abstract
@@ -41,9 +41,13 @@ abstract class Mumsys_Xml_Abstract
     public static function attributesCreate( array $array = array() )
     {
         $sum = array();
-        foreach ($array as $key => &$value) {
-            if (!is_scalar($value)) {
-                $msg = sprintf('Invalid attribute value for key: "%1$s": "%2$s"', $key, gettype($value));
+        foreach ($array as $key => &$value)
+        {
+            if ( !is_scalar($value) ) {
+                $msg = sprintf(
+                    'Invalid attribute value for key: "%1$s": "%2$s"', $key,
+                    gettype($value)
+                );
                 throw new Mumsys_Xml_Exception($msg);
             }
 
