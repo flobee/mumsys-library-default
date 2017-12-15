b67b8cb9fd07d80e5652531d40ad859abcfba722
flobee <flobee@debian>
2017-12-15 21:27:26 +0100

    Improves CS, phpdoc

--- src/Mumsys_Mvc_Controller_Backend.php
+++ src/Mumsys_Mvc_Controller_Backend.php
@@ -1,23 +1,19 @@
 <?php
 
-/*{{{*/
 /**
- * ----------------------------------------------------------------------------
  * Mumsys_Mvc_Controller_Backend
  * for MUMSYS Library for Multi User Management System (MUMSYS)
- * ----------------------------------------------------------------------------
+ *
  * @author Florian Blasel <flobee.code@gmail.com>
  * @copyright Copyright (c) 2011 by Florian Blasel for FloWorks Company
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
- * ----------------------------------------------------------------------------
+ *
  * @category    Mumsys
- * @package     Mumsys_Library
- * @subpackage  Mumsys_Mvc
+ * @package     Library
+ * @subpackage  Mvc
  * @version     1.0.0
  * Created: 2014-01-10
- * @filesource
  */
-/*}}}*/
 
 
 /**
