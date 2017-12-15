775f382615efa358fff4ef5848b2088ff94ae370
flobee <flobee@debian>
2017-12-15 20:42:19 +0100

    Improves CS, phpdoc

--- src/Mumsys_Exception.php
+++ src/Mumsys_Exception.php
@@ -1,36 +1,33 @@
 <?php
 
-/*{{{*/
 /**
- * ----------------------------------------------------------------------------
  * Mumsys_Exception
  * for MUMSYS Library for Multi User Management System (MUMSYS)
- * ----------------------------------------------------------------------------
+ *
  * @author Florian Blasel <flobee.code@gmail.com>
- * ----------------------------------------------------------------------------
  * @copyright (c) 2015 by Florian Blasel
- * ----------------------------------------------------------------------------
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
- * ----------------------------------------------------------------------------
+ *
  * @category    Mumsys
- * @package     Mumsys_Library
+ * @package     Library
  * @version 0.1 - Created on 2009-11-27
  * $Id: Mumsys_Exception.php 3165 2015-04-09 20:25:23Z flobee $
  */
-/*}}}*/
 
 
 /**
  * Generic exception class
  *
  * @category    Mumsys
- * @package     Mumsys_Library
+ * @package     Library
  */
-class Mumsys_Exception extends Exception {
+class Mumsys_Exception
+    extends Exception
+{
     /**
      * Default error code for technical errors, no futher reason as discribed
      * in the error message.
      */
-     const ERRCODE_DEFAULT = 1;
-}
+    const ERRCODE_DEFAULT = 1;
 
+}
