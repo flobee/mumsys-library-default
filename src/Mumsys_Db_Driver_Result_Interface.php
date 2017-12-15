7daf3a6b7b430c4caf6eb7cb2d5b9eca33b152c6
flobee <flobee@debian>
2017-12-15 20:38:39 +0100

    Improves CS

--- src/Mumsys_Db_Driver_Result_Interface.php
+++ src/Mumsys_Db_Driver_Result_Interface.php
@@ -3,14 +3,14 @@
 /**
  * Mumsys_Db_Driver_Result_Interface
  * for MUMSYS Library for Multi User Management System (MUMSYS)
- * ----------------------------------------------------------------------------
- * @author Florian Blasel <flobee.code@gmail.com>
- * @copyright Copyright (c) 2009 by Florian Blasel for FloWorks Company
+ *
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
- * ----------------------------------------------------------------------------
+ * @copyright Copyright (c) 2009 by Florian Blasel for FloWorks Company
+ * @author Florian Blasel <flobee.code@gmail.com>
+ *
  * @category    Mumsys
- * @package     Mumsys_Library
- * @subpackage  Mumsys_Db
+ * @package     Library
+ * @subpackage  Db
  * @version     3.0.1
  */
 
@@ -19,8 +19,8 @@
  * Interface Mumsys_Db_Driver_Result_Interface
  *
  * @category    Mumsys
- * @package     Mumsys_Library
- * @subpackage  Mumsys_Db
+ * @package     Library
+ * @subpackage  Db
  */
 interface Mumsys_Db_Driver_Result_Interface
 {
