c5d44dad0461b33e7de6d55097e804734c8132c6
flobee <flobee@debian>
2017-12-15 22:20:10 +0100

    Improves CS, phpdoc

--- src/Mumsys_Variable_Abstract.php
+++ src/Mumsys_Variable_Abstract.php
@@ -1,20 +1,18 @@
 <?php
 
-/*{{{*/
 /**
  * Mumsys_Variable_Abstract
  * for MUMSYS Library for Multi User Management System (MUMSYS)
- * ----------------------------------------------------------------------------
+ *
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
  * @copyright Copyright (c) 2006 by Florian Blasel for FloWorks Company
  * @author Florian Blasel <flobee.code@gmail.com>
- * ----------------------------------------------------------------------------
+ *
  * @category    Mumsys
  * @package     Library
  * @subpackage  Variable
  * Created: 2006 based on Mumsys_Field, renew 2016 PHP >= 7
  */
-/*}}}*/
 
 
 /**
