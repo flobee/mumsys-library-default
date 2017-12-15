0e5f854a553e02f2bde83e2b75d9567d32fd3211
flobee <flobee@debian>
2017-12-15 20:39:21 +0100

    Improves CS, phpdoc

--- src/Mumsys_Db_Exception.php
+++ src/Mumsys_Db_Exception.php
@@ -1,17 +1,16 @@
 <?php
 
-/*{{{*/
 /**
  * Mumsys_Db_Exception
  * for MUMSYS Library for Multi User Management System (MUMSYS)
- * ----------------------------------------------------------------------------
+ *
  * @author Florian Blasel <flobee.code@gmail.com>
  * @copyright Copyright (c) 2011 by Florian Blasel for FloWorks Company
  * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
- * ----------------------------------------------------------------------------
+ *
  * @category    Mumsys
- * @package     Mumsys_Library
- * @subpackage  Mumsys_Db
+ * @package     Library
+ * @subpackage  Db
  * @version     3.0.0
  */
 /* }}} */
@@ -21,8 +20,8 @@
  * Generic exception for all Db objects.
  *
  * @category    Mumsys
- * @package     Mumsys_Library
- * @subpackage  Mumsys_Db
+ * @package     Library
+ * @subpackage  Db
  */
 class Mumsys_Db_Exception extends Mumsys_Exception
 {
