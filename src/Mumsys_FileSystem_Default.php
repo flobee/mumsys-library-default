425cd8d7416818dc2e2fb8564d7c679f6d7ddf67
flobee <flobee@debian>
2017-12-26 20:04:27 +0100

    Init Mumsys_FileSystem_Default

---
+++ src/Mumsys_FileSystem_Default.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Mumsys_FileSystem_Default
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ *
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright (c) 2006 by Florian Blasel
+ * @author Florian Blasel <flobee.code@gmail.com>
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  FileSystem
+ */
+
+
+/**
+ * Class for the file system to handle files or directories. Default version
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  FileSystem
+ */
+class Mumsys_FileSystem_Default
+    extends Mumsys_FileSystem
+    implements Mumsys_FileSystem_Interface
+{
+    /**
+     * Version ID information.
+     */
+    const VERSION = '3.0.9';
+
+}
