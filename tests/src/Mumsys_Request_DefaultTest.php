85024aa9108ad0aab4e875c8765be488e2f96f3c
flobee <flobee@r61>
2018-01-14 15:54:31 +0100

    Adds version check tests

--- tests/src/Mumsys_Request_DefaultTest.php
+++ tests/src/Mumsys_Request_DefaultTest.php
@@ -26,13 +26,30 @@ class Mumsys_Request_DefaultTest
      */
     protected $_inputPost;
 
-    
+    /**
+     * @var string
+     */
+    protected $_version;
+
+    /**
+     * @var array
+     */
+    protected $_versions;
+
+
     /**
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
     protected function setUp()
     {
+        $this->_version = '1.1.0';
+        $this->_versions = array(
+            'Mumsys_Request_Abstract' => '1.0.1',
+            'Mumsys_Request_Default' => '1.1.0',
+            'Mumsys_Abstract' => '3.0.2',
+        );
+
         $options = array(
             'programKey' => 'programTest',
             'controllerKey' => 'controllerTest',
@@ -82,13 +99,20 @@ class Mumsys_Request_DefaultTest
         $_COOKIE = array('HIDDEN' => 'COOK');
 
         $this->_object = new Mumsys_Request_Default($options);
+        $this->assertInstanceOf('Mumsys_Request_Default', $this->_object);
     }
+
 
     /**
      * @covers Mumsys_Request_Default::getInputPost
+     * @covers Mumsys_Request_Default::setInputPost
      */
-    public function testGetInputPost()
+    public function testGetSetInputPost()
     {
+        $this->_object->setInputPost('programTest', 'prg');
+        $this->_object->setInputPost('notExists', '123');
+        $this->_object->setInputPost('notExists', null);// 4 CC
+
         $actual1 = $this->_object->getInputPost();
         $actual2 = $this->_object->getInputPost('programTest');
         $actual3 = $this->_object->getInputPost('notExists', 123);
@@ -98,11 +122,17 @@ class Mumsys_Request_DefaultTest
         $this->assertEquals('123', $actual3);
     }
 
+
     /**
      * @covers Mumsys_Request_Default::getInputGet
+     * @covers Mumsys_Request_Default::setInputGet
      */
-    public function testGetInputGet()
+    public function testGetSetInputGet()
     {
+        $this->_object->setInputGet('programTest', 'prg');
+        $this->_object->setInputGet('notExists', '123');
+        $this->_object->setInputGet('notExists', null);// 4 CC
+
         $actual1 = $this->_object->getInputGet();
         $actual2 = $this->_object->getInputGet('programTest');
         $actual3 = $this->_object->getInputGet('notExists', 123);
@@ -112,4 +142,10 @@ class Mumsys_Request_DefaultTest
         $this->assertEquals('123', $actual3);
     }
 
+    public function testVersions()
+    {
+         $this->assertEquals($this->_version, Mumsys_Request_Default::VERSION);
+
+         $this->_checkVersionList($this->_object->getVersions(), $this->_versions);
+    }
 }
