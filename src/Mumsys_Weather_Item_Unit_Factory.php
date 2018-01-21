44d8b112379c77e38f26209658c413e6ce8eb296
flobee <flobee@r61>
2018-01-21 16:17:54 +0100

    Init Mumsys_Weather_Item_Unit_Factory

---
+++ src/Mumsys_Weather_Item_Unit_Factory.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Mumsys_Weather_Item_Unit_Default
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ *
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2013 by Florian Blasel for FloWorks Company
+ * @author Florian Blasel <flobee.code@gmail.com>
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Weather
+ */
+
+/**
+ * Unit item factory. Loader for specific implementations.
+ * *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Weather
+ */
+class Mumsys_Weather_Item_Unit_Factory
+{
+    /**
+     * Version ID information.
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * Return new unit item by given name.
+     * @param string $name Name of the required implementation; Default:
+     * 'Default'
+     * @param array $input List of key/values pairs for the construction of the
+     * item
+     *
+     * @return \Mumsys_Weather_Item_Unit_Interface Unit item interface
+     * @throws Mumsys_Weather_Exception
+     */
+    /**
+     *
+     * @param string $name
+     * @return \Mumsys_Weather_Item_Unit_Interface
+     */
+    public static function createItem(string $name = null, array $input = array() ):Mumsys_Weather_Item_Unit_Interface
+    {
+        if ( $name === null ) {
+            $name = 'Default';
+        }
+
+        $classname = 'Mumsys_Weather_Item_Unit_' . $name;
+
+        if ( ctype_alnum( $name ) === false ) {
+            $mesg = sprintf(
+                'Invalid characters in class name "%1$s"', $classname
+            );
+            throw new Mumsys_Weather_Exception( $mesg );
+        }
+
+        return new $classname($input);
+    }
+
+}
