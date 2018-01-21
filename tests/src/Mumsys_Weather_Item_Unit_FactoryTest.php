8b7ea29f8df15935d021d00962eb3ffef3e8a11b
flobee <flobee@r61>
2018-01-21 18:25:28 +0100

    Adds copyright information

--- tests/src/Mumsys_Weather_Item_Unit_FactoryTest.php
+++ tests/src/Mumsys_Weather_Item_Unit_FactoryTest.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * Mumsys_Weather_Item_Unit_FactoryTest
+ * for MUMSYS Library for Multi User Management System (MUMSYS)
+ *
+ * @license LGPL Version 3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright Copyright (c) 2013 by Florian Blasel
+ * @author Florian Blasel <flobee.code@gmail.com>
+ *
+ * @category    Mumsys
+ * @package     Library
+ * @subpackage  Weather
+ * @verion      1.0.0
+ * Created: 2013, renew 2018
+ */
 
 /**
  * Mumsys_Weather_Item_Unit_Factory Test
