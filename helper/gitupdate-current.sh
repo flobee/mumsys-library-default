d24e7a3094477b573262f2a2964400f34708a9e3
flobee <flobee@r61>
2018-01-27 19:02:52 +0100

    Updates docs

--- helper/gitupdate-current.sh
+++ helper/gitupdate-current.sh
@@ -5,7 +5,7 @@ echo '+-----------------';
 echo '| Update code:    ';
 git pull;
 
-
+# Comment the following lines if your are NOT using VIM
 echo '+-----------------';
 echo '| Updating ctags  ';
 ctags-exuberant -f .php.ctags --languages=+PHP,-JavaScript,-HTML,-Perl,-Sh,-Sql,-Scheme -R src/ tests/
