fe184c3604b67d8ec79b417d39e44f619136c1ad
flobee <flobee@r61>
2018-01-27 19:01:33 +0100

    Updates docs

--- helper/gitupdate-current.sh
+++ helper/gitupdate-current.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+
 echo '+-----------------';
 echo '| Update code:    ';
 git pull;
 
+
 echo '+-----------------';
 echo '| Updating ctags  ';
-ctags-exuberant -f php.ctags --languages=+PHP,-JavaScript,-HTML,-Perl,-Sh,-Sql,-Scheme -R src/ tests/
+ctags-exuberant -f .php.ctags --languages=+PHP,-JavaScript,-HTML,-Perl,-Sh,-Sql,-Scheme -R src/ tests/
+
 
 echo 'done.';
 
