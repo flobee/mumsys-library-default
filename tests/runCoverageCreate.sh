a834b8a3e48a4bcc1690b9d8f5a744769112e468
flobee <flobee@debian.local>
2019-08-29 22:21:55 +0200

    Updates tests scripts

--- tests/runCoverageCreate.sh
+++ tests/runCoverageCreate.sh
@@ -6,4 +6,6 @@ echo "------------------------------------------";
 echo "usage: $0 [phpunit options] <file or dir>";
 echo "------------------------------------------";
 
-php ../vendor/bin/phpunit  --colors --configuration phpunit-coverage.xml --bootstrap ./bootstrap.php $*
+_DIR=$(dirname "$0");
+
+php $_DIR/../vendor/bin/phpunit  --colors --configuration $_DIR/phpunit-coverage.xml --bootstrap $_DIR/bootstrap.php $*
