8fbe0eb02845b9ae26af11fe542d2dbfd8acdccb
flobee <flobee@debian.local>
2019-11-09 01:48:50 +0100

    Updates testing/ build/ dependency tools
    + Removes local runners
    + Link runner's from misc/ repository

--- tests/phpcs-base.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# ../vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/
-
-STANDARD="--standard=${CUR_DIR}/../misc/coding/Mumsys"
-IGNORELINE='--ignore=data/*,vendor/*,helper/*,tmp/*,misc/*'
