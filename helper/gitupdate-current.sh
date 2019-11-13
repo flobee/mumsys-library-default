6e8cd37464e05522681d3791f4974f8a78667ae7
flobee <flobee@debian.local>
2019-11-13 22:47:17 +0100

    Improves helper/gitupdate-current force update ctag changes

--- helper/gitupdate-current.sh
+++ helper/gitupdate-current.sh
@@ -8,6 +8,9 @@ git pull;
 # Comment the following lines if your are NOT using VIM
 echo '+-----------------';
 echo '| Updating ctags  ';
+if [ -f .php.ctags ]; then
+    rm -f .php.ctags;
+fi
 ctags-exuberant -f .php.ctags --languages=+PHP,-JavaScript,-HTML,-Perl,-Sh,-Sql,-Scheme -R src/ tests/
 
 
